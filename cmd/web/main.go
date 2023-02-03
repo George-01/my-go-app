5c0cd7f284d26a9b92c8a87c7a97bc779e3091c9
George-01 <38787656+George-01@users.noreply.github.com>
2023-02-03 16:10:02 +0000

    Update main.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -17,3 +17,39 @@ func main() {
 	fmt.Printf("Starting application on port %s", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
 }
+
+
+/*
+func CreateData(rw http.ResponseWriter, r *http.Request) {
+	logger.Info("User called the createData method")
+
+	mutex.Lock()
+	defer mutex.Unlock()
+		
+	if !validations.ValidateRequestMethod(rw, r, http.MethodPost){
+		return
+	}
+	
+	decoder := json.NewDecoder(r.Body)
+	var newProducts map[string]interface{}
+	err := decoder.Decode(&newProducts)
+	if err != nil {
+		fmt.Fprintf(rw, "Error decoding update request: %v", err)
+		logger.Error(fmt.Sprintf("Error decoding update request: %v", err))
+		return
+	}
+
+	for key, value := range newProducts{
+
+		if _, ok := kvStore.Products[key]; ok{
+			rw.WriteHeader(http.StatusConflict)
+			fmt.Fprintf(rw, "This key: %v already exist\n", key)
+			logger.Error(fmt.Sprintf("This key: %v already exist\n", key))
+			return
+		} 		
+		kvStore.Products[key] = value
+	}
+	rw.WriteHeader(http.StatusCreated)
+	logger.Info("User created new items")
+}
+*/
